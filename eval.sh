e5f40043e516fb658ac602530e07aacc04212328
Mike Percy <mpercy@cloudera.com>
2015-06-07 17:15:56 -0700

    Add evaluation script

--- eval.sh
+++ eval.sh
@@ -1,15 +1,9 @@
-#!/bin/bash
-DATA_DIR=$(basename "$1") # strip leading / trailing slashes
-if [ ! -d "$DATA_DIR" ]; then
-  echo "Usage: $0 data_dir"
-  exit 1
-fi
+#!/bin/bash -xe
 
-BASE=$(dirname $(readlink -f $0))
-
-set -o pipefail # pass any errors through back to the shell
-set -x
-
-cd $DATA_DIR
-$BASE/main.py $DATA_DIR $BASE/20news-18828 2>&1 | tee run_log_$(date '+%Y%m%d-%H%M%S').log
-echo "Python retcode: $?"
+for model_data in glove.6B.100d.txt glove.6B.300d.txt; do
+  for featurizer in MaxTopicFeatureExtractor HierarchicalTopicFeatureExtractor FlatFeatureExtractor; do
+    for max_regions in 1 5 15 30 50; do
+      ./main.py --featurizer $featurizer --max_regions $max_regions $model_data 20news-18828/ records.txt
+    done
+  done
+done
